5042b7c90dabf6a99ab3e6346875a551630c9dba
moziqi <709847739@qq.com>
2014-09-15 21:14:48 +0800

    设计了2张表数据

--- src/main/java/org/mo/jims/coop/repository/UserListRepository.java
+++ src/main/java/org/mo/jims/coop/repository/UserListRepository.java
@@ -1,8 +1,23 @@
 package org.mo.jims.coop.repository;
 
+import java.util.List;
+
+import org.mo.jims.coop.entity.UserList;
 import org.springframework.stereotype.Repository;
 
 @Repository(value = "userListRepository")
 public interface UserListRepository {
+	public UserList selectById(String id);
 
+	public List<UserList> selectAll();
+
+	public boolean insert(UserList entity);
+
+	public boolean update(UserList entity);
+
+	public boolean updateById(String id);
+
+	public boolean delete(UserList entity);
+
+	public boolean deleteById(UserList id);
 }
